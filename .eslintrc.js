56e2724991e0a2737b25b9dafd3d5ad3f310c32a
ChristophWalter <ChristophWalter@users.noreply.github.com>
2019-10-24 15:19:55 +0200

    Add eslint plugin for gridsome queries

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,12 @@ module.exports = {
   env: {
     node: true
   },
+  plugins: ["gridsome"],
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "gridsome/format-query-block": "warn"
   },
   parserOptions: {
     parser: "babel-eslint"
