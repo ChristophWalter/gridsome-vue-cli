be3212162e90c437fe99db20802134de1ba4f7e6
ChristophWalter <ChristophWalter@users.noreply.github.com>
2019-10-24 15:19:29 +0200

    Add example for static-query using webpack loader from gridsome

---
+++ src/components/GridsomeStaticExample.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="gridsome-content">
+    <h1>Gridsome via vue-cli</h1>
+    <p>
+      Characters from
+      <a href="https://rickandmortyapi.com/graphql"
+        >https://rickandmortyapi.com/graphql</a
+      >
+    </p>
+    <ul>
+      <li v-for="character in characters" :key="character.id">
+        {{ character.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<static-query>
+{
+  rickandmorty {
+    characters {
+      results {
+        id
+        name
+      }
+    }
+  }
+}
+</static-query>
+
+<script>
+export default {
+  name: "GridsomeStaticExample",
+  computed: {
+    characters() {
+      return this.$static.data.rickandmorty.characters.results;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.gridsome-content {
+  text-align: left;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,28 @@
+function createGraphQLRule(config, type, loader) {
+  const re = new RegExp(`blockType=(${type})`);
+
+  config.module
+    .rule(type)
+    .resourceQuery(re)
+    .use("babel-loader")
+    .loader("babel-loader")
+    .options({
+      presets: [require.resolve("@vue/babel-preset-app")]
+    })
+    .end()
+    .use(`${type}-loader`)
+    .loader(require.resolve(loader))
+    .options({
+      gridsomeGraphqlEndpoint: "http://localhost:8080/___graphql"
+    });
+}
+
+module.exports = {
+  chainWebpack: config => {
+    createGraphQLRule(
+      config,
+      "static-query",
+      "gridsome/lib/plugins/vue-components/lib/loaders/static-query.js"
+    );
+  }
+};
